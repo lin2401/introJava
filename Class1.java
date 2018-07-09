00da6381c663d61bf434b77ba40bc14ff8b0b82a
lin2401 <dahoua.fatima@gmail.com>
2018-07-09 15:04:01 +0200

    fistday firstcommit

---
+++ Class1.java
@@ -0,0 +1,31 @@
+
+public class Class1 {
+
+	public static void main(String[] args) {
+System.out.print("hello");
+	}
+//chiffre
+	// Entiers du plus grand au plus petit 
+	long monLong = 0;
+	int monEntier = 1; //99%
+	short monShort = 2;
+	byte monByte = 3;
+
+//flot 
+	double monDouble = 3.5;
+	
+	float monFloat = (float)6.3;
+
+	
+	//bool
+	boolean monBoolean = true;  // true ou false 
+
+// char char monChar = 'v';
+	
+
+
+}
+
+
+
+
